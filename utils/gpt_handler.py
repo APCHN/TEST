95421933f2ad29b1d0dd2ab4e85df39c6f543453
APCHN <quan.duong@caodangvietmy.edu.vn>
2025-01-15 11:20:44 +0700

    Update gpt_handler.py

--- utils/gpt_handler.py
+++ utils/gpt_handler.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 # Load API key từ file .env
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = os.getenv("Osk-proj-eO8j8dGwQTaaf_5k3t5FDIP_1jJx57GNZkO7sfikueTO3V4UvKB-7859mr4K-FsOSaLjnMZ8J5T3BlbkFJvutoUi4o8APZyv-DOizJ-al08BQv-QoxSLz9_vtDlo45sfKvT4qqozXT6FCSfsilzsLwlDgEUA")
 
 def ask_gpt(messages, model="gpt-3.5-turbo"):
     """
